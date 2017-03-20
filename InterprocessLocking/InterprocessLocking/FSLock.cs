3c0889bb6b917ee2276051c25a4e47aa73abff88
Tolga Evcimen <h.tolgaevcimen@gmail.com>
2017-03-20 11:05:11 +0300

    first commit of the project

---
+++ InterprocessLocking/InterprocessLocking/FSLock.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.IO;
+using System.Threading;
+
+namespace InterprocessLocking
+{
+    public class FSLock : IDisposable
+    {
+        public int TryoutPeriod = 30;
+
+        FileStream LockedStream { get; set; }
+
+        public FSLock ( string handle )
+        {
+            while ( LockedStream == null )
+            {
+                try
+                {
+                    //lock will be attempted to be acquired
+                    LockedStream = new FileStream(handle, FileMode.OpenOrCreate, FileAccess.ReadWrite, FileShare.None);
+                    //lock acquired
+                }
+                catch ( IOException )
+                {
+                    // could not acquire the lock, wait
+                    Thread.Sleep(TryoutPeriod);
+                }
+                catch
+                {
+                    throw;
+                }
+            }
+        }
+
+        public void Dispose ()
+        {
+            // lock will be disposed
+            LockedStream.Close();
+            // lock disposed
+        }
+    }
+}
---
+++ InterprocessLocking/InterprocessLocking/MutexLock.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Security.AccessControl;
+using System.Security.Principal;
+using System.Threading;
+
+namespace InterprocessLocking
+{
+    public class MutexLock : IDisposable
+    {
+        Mutex _Mutex { get; set; }
+        bool HasHandle { get; set; }
+
+        public MutexLock ( string handle )
+        {
+            var createdNew = false;
+            try
+            {
+                // avoid race condition for security reasons
+                var allowEveryoneRule = new MutexAccessRule(new SecurityIdentifier(WellKnownSidType.WorldSid, null), MutexRights.FullControl, AccessControlType.Allow);
+                var securitySettings = new MutexSecurity();
+                securitySettings.AddAccessRule(allowEveryoneRule);
+
+                _Mutex = new Mutex(false, "Global:\\" + handle, out createdNew, securitySettings);
+                HasHandle = _Mutex.WaitOne();
+            }
+            catch ( AbandonedMutexException )
+            {
+                HasHandle = true;
+            }
+        }
+
+        public void Dispose ()
+        {
+            if ( _Mutex == null ) return;
+            // lock will be disposed
+            if ( HasHandle )
+                _Mutex.ReleaseMutex();
+            _Mutex.Dispose();
+            // lock disposed
+        }
+    }
+}
